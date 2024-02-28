44957535deb8a64b76c09cfad217bb1fe991b840
ramses-Rodriguez <161273874+ramses-Rodriguez@users.noreply.github.com>
2024-02-27 22:12:28 -0800

    Add files via upload

---
+++ 02-February/Week-04/exercise-01/exercise-01.js
@@ -0,0 +1,8 @@
+// Solicita edad al usuario
+let age = prompt("Type your age:", 0);
+
+// Realiza una suma de la edad ingresada
+let newAge = parseInt(age) + 10;
+
+// Muestra el mensaje en la consola
+console.log("Your age in ten year is:", newAge);
